628bf619c01d4e8fe19c29dac3c8c6103c33d6cc
Ecuster666 <85421789+Ecuster666@users.noreply.github.com>
2022-08-02 09:12:26 +0800

    Update FileTest.java

--- ggkt_parent/service/service_vod/src/test/java/com/atguigu/FileTest.java
+++ ggkt_parent/service/service_vod/src/test/java/com/atguigu/FileTest.java
@@ -18,8 +18,8 @@ public class FileTest {
     public static void main(String[] args) {
         // 1 初始化用户身份信息（secretId, secretKey）。
         // SECRETID和SECRETKEY请登录访问管理控制台 https://console.cloud.tencent.com/cam/capi 进行查看和管理
-        String secretId = "AKIDJWXO9i294rRolW1JRN4Zt41jEGAtPjqk";
-        String secretKey = "jIgpkU3NUN2YrwnTzIROAi3B6oMipkSw";
+        String secretId = "";
+        String secretKey = "";
         COSCredentials cred = new BasicCOSCredentials(secretId, secretKey);
         // 2 设置 bucket 的地域, COS 地域的简称请参照 https://cloud.tencent.com/document/product/436/6224
         // clientConfig 中包含了设置 region, https(默认 http), 超时, 代理等 set 方法, 使用可参见源码或者常见问题 Java SDK 部分。
